b8c2b0862fa36d4e82d69c1f5c50008b55c3509d
nadianafi <nadianafifazillah@smkwikrama.sch.id>
2023-08-22 14:34:23 +0700

    file baru1

--- 4.php
+++ 4.php
@@ -1,46 +1,27 @@
-<?php
-$waktu; $jam; $menit; $detik; $hasil = "";
-?>
-
 <!DOCTYPE html>
-<html lang="en">
-
+<html>
 <head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Konversi Detik ke Jam, Menit, Detik</title>
 </head>
 <body>
-    <form action="" method="post">
-        <div style="display: flex;">
-            <label for="times">Masukan Total Detik</label>
-            <input type="number" name="times" id="times">
-        </div>
-        <button type="submit" name="submit">Kirim</button>
+    <form method="post" action="">
+        <label for="totalDetik">Total Detik:</label>
+        <input type="number" id="totalDetik" name="totalDetik" required><br>
+
+        <button type="submit">Konversi</button>
     </form>
+
+    <?php
+    if ($_SERVER["REQUEST_METHOD"] == "POST") {
+         $totalDetik = (int) $_POST["totalDetik"];
+
+         $jam = floor($totalDetik / 3600);
+         $sisaDetik = $totalDetik % 3600;
+         $menit = floor($sisaDetik / 60);
+         $detik = $sisaDetik % 60;
+
+         echo "Hasil : $jam jam, $menit menit, $detik detik";
+     }
+    ?>
 </body>
 </html>
-
-<?php
-if(isset($_POST['submit'])){
-    //pengisian input ke variable
-    $waktu = $_POST['times'];
-//decision
-if($waktu >= 3600){
-    // floor untuk membulatkan kebawah, veil membulatkan keatas, round membulatkan keatas dan kebawah
-    $jam = floor($waktu / 3600);
-    $waktu -= ($jam * 3600);
-    $hasil .= $jam . " jam ";
-    // operator += bersifat menambah, kalau hanya = bersifat menimpa
-}
-else if($waktu >= 60){
-    $menit = floor($waktu / 60);
-    $waktu -= ($menit * 60);
-    $hasil .= $menit . " menit ";
-}
-    $detik = $waktu;
-    $hasil .= $detik . " detik "; 
-
-echo $hasil;
-}
-?>
