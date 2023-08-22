a519d6a5ebc3273e371bad8b389c3e6f5a973b2c
nadianafi <nadianafifazillah@smkwikrama.sch.id>
2023-08-22 10:52:18 +0700

    nadia

---
+++ flowchart.php
@@ -0,0 +1,9 @@
+<?php
+   for($no=1;$no<=50;$no++)
+       {
+           if($no % 2 == 0)
+               {echo "$no = Bilangan Genap<br>";}
+           else
+               {echo "$no = Bilangan Ganjil<br>";}
+       }
+?>
